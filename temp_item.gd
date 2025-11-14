1d006938a1a719a3467feda9b18152c0fba370ad
willbill0808 <williamlrosewell08@gmail.com>
2025-11-14 12:19:07 +0100

    action 1

--- temp_item.gd
+++ temp_item.gd
@@ -1,6 +1,6 @@
 extends StaticBody2D
 
-var placeList = ["spawn_node1", "spawn_node2", "spawn_node3"]
+var placeList = ["spawn_node1", "spawn_node2", "spawn_node3", "spawn_node4", "spawn_node5", "spawn_node6"]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
