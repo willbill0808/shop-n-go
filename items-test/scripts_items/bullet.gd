f56b9978c13cc000cf7b71c900c780650b999b21
Dagrate123 <sigia002@osloskolen.no>
2025-11-19 14:16:45 +0100

    collision_fixed

--- items-test/scripts_items/bullet.gd
+++ items-test/scripts_items/bullet.gd
@@ -3,10 +3,11 @@ extends Area2D
 var speed = 750
 
 func _physics_process(delta):
-	position += transform.x * speed * delta
+	position += transform.x * speed * delta  
 
-func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("shootable"):
-		body.queue_free()
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("shootable"):
+		area.queue_free()
 		queue_free()
-		print("shootable")    
+		print("shootable")  
---
+++ items-test/scripts_items/ice_object.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+var speed = 750
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position -= transform.x * speed * delta  
+
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("shootable"):
+		area.queue_free()
+		queue_free()
+		print("shootable")  
