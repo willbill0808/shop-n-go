30e7b6d02d8d3f97ac105d0e5f556657899247ef
Dagrate123 <sigia002@osloskolen.no>
2025-11-19 14:29:54 +0100

    Update ice_object.gd

--- items-test/scripts_items/ice_object.gd
+++ items-test/scripts_items/ice_object.gd
@@ -14,6 +14,7 @@ func _process(delta: float) -> void:
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("shootable"):
+		#karakter sin movement men mer freaky
 		area.queue_free()
 		queue_free()
 		print("shootable")  
