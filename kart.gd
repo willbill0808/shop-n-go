73c6acb0f35308160adafb18c4ba05b7f0d4c585
willbill0808 <williamlrosewell08@gmail.com>
2025-11-21 11:20:55 +0100

    fre fina

--- kart.gd
+++ kart.gd
@@ -1,21 +1,23 @@
-extends Node2D
+extends Node
 
 func _ready():
+	# Get the world
 	var world: World2D = $World.get_world_2d()
-	
+
+	# Get SubViewports
 	var left_vp = $UI/SubViewportContainer_Left/SubViewport_Left
 	var right_vp = $UI/SubViewportContainer_Right/SubViewport_Right
 
+	# Assign the same world to both SubViewports
 	left_vp.world_2d = world
 	right_vp.world_2d = world
 
-	# Move cameras into viewports
+	# Move Player1 camera into left SubViewport
 	var cam_left = $World/Player1/Camera2D_Left
 	cam_left.get_parent().remove_child(cam_left)
 	left_vp.add_child(cam_left)
-	cam_left.current = true
 
+	# Move Player2 camera into right SubViewport
 	var cam_right = $World/Player2/Camera2D_Right
 	cam_right.get_parent().remove_child(cam_right)
 	right_vp.add_child(cam_right)
-	cam_right.current = true
