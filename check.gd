c98b18a1e71109ad654f1f7ee45f5e90ae8eeb15
willbill0808 <williamlrosewell08@gmail.com>
2025-11-27 13:49:08 +0100

    thur com

--- check.gd
+++ check.gd
@@ -1,8 +1,11 @@
 extends Area2D
 
-signal passed(nameSake)
+var selfpassed = false
+
+signal passed(nameSake, passSake)
 var body_name
 
 func _on_body_entered(body: Node2D) -> void:
 	print (body.name)
-	emit_signal("passed", body.name)
+	var bodName = body.name
+	emit_signal("passed", bodName, name)
--- kart.gd
+++ kart.gd
@@ -1,32 +1,14 @@
 extends Node
 
+var player1Round = 0
+var player2Round = 0
+
 func _ready():
-	# Get the world
-	var world: World2D = $World.get_world_2d()
 
-	# Get SubViewports
-	var left_vp = $UI/SubViewportContainer_Left/SubViewport_Left
-	var right_vp = $UI/SubViewportContainer_Right/SubViewport_Right
-
-	# Assign the same world to both SubViewports
-	left_vp.world_2d = world
-	right_vp.world_2d = world
-
-	# Move Player1 camera into left SubViewport
-	var cam_left = $World/Player1/Camera2D_Left
-	cam_left.get_parent().remove_child(cam_left)
-	left_vp.add_child(cam_left)
-
-	# Move Player2 camera into right SubViewport
-	var cam_right = $World/Player2/Camera2D_Right
-	cam_right.get_parent().remove_child(cam_right)
-	right_vp.add_child(cam_right)
-	
-	
-	var passname = get_parent().find_child("check")
-	passname.connect("passed", Callable(self, "_passer",))
+	var passname = find_child("check")
+	passname.connect("passed", _passer)
 	
 
-func _passer(sake):
-	print(sake)
-	print("passer")
+func _passer(nameSake, passSake):
+	print("nameSake:", nameSake)
+	print("passSake:", passSake)
