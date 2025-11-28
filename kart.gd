81c71eb5db5106d5ea7032ebef4f156b4759bf3f
willbill0808 <williamlrosewell08@gmail.com>
2025-11-28 08:30:22 +0100

    checks 2

--- kart.gd
+++ kart.gd
@@ -3,6 +3,11 @@ extends Node
 var player1Round = 0
 var player2Round = 0
 
+var player1check1 = false
+var player1check2 = true
+var player2check1 = false
+var player2check2 = true
+
 func _ready():
 
 	var passname = find_child("check")
@@ -15,3 +20,27 @@ func _ready():
 func _passer(nameSake, passSake):
 	print("nameSake:", nameSake)
 	print("passSake:", passSake)
+	
+	if nameSake == "Player1":
+		if passSake == "check":
+			if player1check2 == true:
+				player1check1 = true
+				player1check2 = false
+				
+				print("play1: ", player1check1, player1check2)
+		else:
+			if player1check1 == true:
+				player1check2 = true
+				player1check1 = false
+				
+				print("play1: ", player1check1, player1check2)
+
+	else:
+		if passSake == "check":
+			if player2check2 == true:
+				player2check1 = true
+				player2check2 = false
+		else:
+			if player2check1 == true:
+				player2check2 = true
+				player2check1 = false
