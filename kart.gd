acf674b96620911f8d2ad516a130d3bd0dc27435
willbill0808 <williamlrosewell08@gmail.com>
2025-11-28 08:19:10 +0100

    checkpoints

--- kart.gd
+++ kart.gd
@@ -8,6 +8,9 @@ func _ready():
 	var passname = find_child("check")
 	passname.connect("passed", _passer)
 	
+	var passname2 = find_child("check2")
+	passname2.connect("passed", _passer)
+	
 
 func _passer(nameSake, passSake):
 	print("nameSake:", nameSake)
